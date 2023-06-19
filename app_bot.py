50e30fcc5d9a00af50c624a5d934c226962a3287
DanielIhrmark <117570795+DanielIhrmark@users.noreply.github.com>
2023-06-20 00:05:19 +0200

    Update app_bot.py

--- app_bot.py
+++ app_bot.py
@@ -7,7 +7,7 @@ openai.api_key = "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"
 #Setting up AI prompt
 def generate_response(prompt):
     completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", 
-                                          messages=[{"role": "system", "content": "You are a librarian."},
+                                          messages=[{"role": "system", "content": "You are a helpful and happy librarian."},
                                                     {"role": "user", "content": prompt}])
 
     return completion.choices[0].message.content
